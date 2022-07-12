77853d27359c9e6328f7c783999829aa3ef77a04
Vladimir Torres Muñoz <102809673+VladimirForsend@users.noreply.github.com>
2022-07-12 14:02:31 -0400

    nav mobile add hamburguer button

--- wp-content/themes/comercio3/assets/templates/navs/nav-mobile.php
+++ wp-content/themes/comercio3/assets/templates/navs/nav-mobile.php
@@ -3,7 +3,8 @@
     <div class="container-fluid">
         <!---row-->
         <div class="row w-100 d-flex justify-content-between">
-            <!---navbrand--><div class="navbar-brand col-3">
+            <!---navbrand-->
+            <div class="navbar-brand col-3">
                 <?php
                 the_custom_logo();
                 if (is_front_page() && is_home()) :
@@ -21,12 +22,21 @@
                     <p class="site-description"><?php echo $fundaciones_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped 
                                                 ?></p>
                 <?php endif; ?>
-                </div>
-          
 
-            <button class="navbar-toggler col-2" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
+            </div>
+            <div class="contenedor-categorias-boton col-6 col-md-2">
+                <a tabindex="0" class="d-flex align-items-center" data-bs-html="true" role="button" data-bs-container="body" data-bs-toggle="popover" data-bs-placement="bottom" data-bs-content='<?php include get_template_directory() . '/assets/templates/navs/arraycat-menu.php'; ?>'>
+                    <button class="boton-menu-desk" type="button">
+                        <div class="boton-hamburguesa">
+                            <span class="linea-hamburguesa"></span>
+                            <span class="linea-hamburguesa"></span>
+                            <span class="linea-hamburguesa"></span>
+                        </div>
+                        <span class="boton-texto">Categorías</span>
+                    </button>
+
+                </a>
+            </div>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
 
                 <?php //OPCIÓN MENU 1 SIMPLE CON BOOTSTRAP
@@ -40,7 +50,9 @@
                 );
                 ?>
 
-<?php if ( function_exists( 'aws_get_search_form' ) ) { aws_get_search_form( true, array( 'id' => '62968e5c35f00' ) ); } ?>
+                <?php if (function_exists('aws_get_search_form')) {
+                    aws_get_search_form(true, array('id' => '62968e5c35f00'));
+                } ?>
             </div>
         </div>
     </div>
