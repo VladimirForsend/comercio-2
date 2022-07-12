51fe09e054195aebecf8a2f037107e21b9e82952
Vladimir Torres Muñoz <102809673+VladimirForsend@users.noreply.github.com>
2022-07-12 18:01:18 -0400

    Update arraycat-menu.php

--- wp-content/themes/comercio3/assets/templates/navs/arraycat-menu.php
+++ wp-content/themes/comercio3/assets/templates/navs/arraycat-menu.php
@@ -1,7 +1,7 @@
 <?php //OPCIÓN MENU 1 SIMPLE CON BOOTSTRAP
                 wp_nav_menu(
                     array(
-                        'theme_location' => 'menu-superior',
+                        'theme_location' => 'menu-categoria',
                         'menu_id'        => 'navbar',
                         'menu_class'     => 'navbar-nav pop-menu',
                         'container_class' => 'nada'
